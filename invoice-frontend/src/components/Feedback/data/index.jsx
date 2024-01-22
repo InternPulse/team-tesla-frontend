096dba79c343524dfb9b1805d967a95ab97d648d
AgonoDavid <davidagono239@gmail.com>
2024-01-22 03:57:39 +0100

    Updated Data structure for Reviews

---
+++ invoice-frontend/src/components/Feedback/data/index.jsx
@@ -0,0 +1,28 @@
+import Proflie from "../../../assets/profile.png";
+
+export const Data = [
+  {
+    id: 1,
+    image: Proflie,
+    name: "James John",
+    location: "Lagos",
+    profilereview:
+      "Lorem ipsum dolor sit amet consectetur. Ac adipiscing porta amet, Nulla nunc tincidunt cursus et est id faucibus. Quam uran",
+  },
+  {
+    id: 2,
+    image: Proflie,
+    name: "James John",
+    location: "Lagos",
+    profilereview:
+      "Lorem ipsum dolor sit amet consectetur. Ac adipiscing porta amet, Nulla nunc tincidunt cursus et est id faucibus. Quam uran",
+  },
+  {
+    id: 3,
+    image: Proflie,
+    name: "James John",
+    location: "Lagos",
+    profilereview:
+      "Lorem ipsum dolor sit amet consectetur. Ac adipiscing porta amet, Nulla nunc tincidunt cursus et est id faucibus. Quam uran",
+  },
+];
--- invoice-frontend/src/components/Feedback/index.jsx
+++ invoice-frontend/src/components/Feedback/index.jsx
@@ -1,57 +1,25 @@
-import Proflie from "../../assets/profile.png";
-
+// import Proflie from "../../assets/profile.png";
+import { Data } from "./data";
 const Feedback = () => {
   return (
-    <div className=" mt-[100px]">
+    <div className=" mt-[90px]">
       <div className=" text-center">
         <p className=" text-2xl">What Invoice Pulse Users Are Saying</p>
       </div>
-      <div className=" grid grid-cols-3 gap-[50px] mt-[40px]">
-        <div className="  p-[20px] flex gap-[10px]">
-          <div className=" w-[100%]">
-            <img src={Proflie} alt="profile" />
+      <div className=" grid grid-cols-3">
+        {Data.map((review) => (
+          <div className="  p-[20px] flex gap-[5px] mt-[30px]" key={review.id}>
+            <div className=" w-[100%]">
+              <img src={review.image} alt="profile" />
+            </div>
+            <div>
+              <p className=" text-[19px] font-medium py-[5px]">{review.name}</p>
+              <p className=" text-[10px]">{review.location}</p>
+              <p className=" text-2xl py-[5px]">----</p>
+              <p className=" text-[12px]">{review.profilereview}</p>
+            </div>
           </div>
-          <div>
-            <p className=" text-[19px] font-medium py-[5px]">James John</p>
-            <p className=" text-[10px]">Lagos</p>
-            <p className=" text-2xl py-[5px]">----</p>
-            <p className=" text-[12px]">
-              Lorem ipsum dolor sit amet consectetur. Ac adipiscing porta amet
-              in. Nulla nunc tincidunt cursus et est id faucibus. Quam urna
-              dignissim ut sed aenean bibendum
-            </p>
-          </div>
-        </div>
-        <div className="  p-[20px] flex gap-[10px]">
-          <div className=" w-[100%]">
-            <img src={Proflie} alt="profile" />
-          </div>
-          <div>
-            <p className=" text-[19px] font-medium py-[5px]">James John</p>
-            <p className=" text-[10px]">Lagos</p>
-            <p className=" text-2xl py-[5px]">----</p>
-            <p className=" text-[12px]">
-              Lorem ipsum dolor sit amet consectetur. Ac adipiscing porta amet
-              in. Nulla nunc tincidunt cursus et est id faucibus. Quam urna
-              dignissim ut sed aenean bibendum
-            </p>
-          </div>
-        </div>
-        <div className="  p-[20px] flex gap-[10px]">
-          <div className=" w-[100%]">
-            <img src={Proflie} alt="profile" />
-          </div>
-          <div>
-            <p className=" text-[19px] font-medium py-[5px]">James John</p>
-            <p className=" text-[10px]">Lagos</p>
-            <p className=" text-2xl py-[5px]">----</p>
-            <p className=" text-[12px]">
-              Lorem ipsum dolor sit amet consectetur. Ac adipiscing porta amet
-              in. Nulla nunc tincidunt cursus et est id faucibus. Quam urna
-              dignissim ut sed aenean bibendum
-            </p>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );

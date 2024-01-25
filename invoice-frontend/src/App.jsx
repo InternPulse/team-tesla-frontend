30b95f00cae6a7fd1ceddfe7c7ff53dbe811163e
aBugInTheSystem <ekom338.com@gmail.com>
2024-01-25 11:37:23 +0100

    Minor change for views

--- invoice-frontend/src/App.jsx
+++ invoice-frontend/src/App.jsx
@@ -12,6 +12,17 @@ import ForgotPassword from "./Pages/ForgotPasswordPage/ForgotPassword";
 import ClientPage from "./Pages/ClientPage/ClientPage";
 
 export default function App() {
+  const isMobile = window.innerWidth < 768;
+
+  if (isMobile) {
+    return (
+      <div>
+        <p className="text-center text-xl flex min-h-screen items-center font-Montserrat">
+          Please use a laptop or desktop device to access this application.
+        </p>
+      </div>
+    );
+  }
   return (
     <Suspense>
       <Routes>
@@ -21,7 +32,7 @@ export default function App() {
           element={<BusinessRegistration />}
           path="/business-registration"
         />
-        <Route element={<UserRegistration />} path="user-registration" />
+        <Route element={<UserRegistration />} path="/user-registration" />
         <Route element={<LoginPage />} path="/login" />
         <Route element={<ForgotPassword />} path="/forgotPassword" />
         <Route element={<Authentication />} path="/auth" />
